2c96f0af661463aff77e3510f32fede4c4d184f1
Arjun-G-Ravi <arjungravi1@gmail.com>
2023-10-01 09:11:53 +0530

    Added some more functions

--- Projects/01_Micrograd/main.py
+++ Projects/01_Micrograd/main.py
@@ -7,9 +7,10 @@ class Value:
         self._backward = lambda: None  # an empty function
 
     def __repr__(self):
-        return f'<Value>(val={self.val}, grad={self.grad})'
+        return f'<Value>(val={self.val}, grad={self.grad})\n'
     
     def __add__(self,other):
+        other = other if isinstance(other, Value) else Value(other)
         out = Value(self.val + other.val, (self, other), '+')
         
         def _backward():
@@ -18,8 +19,11 @@ class Value:
         out._backward = _backward
         return out
     
+    def __radd__(self,other):
+        return self+other
     
     def __mul__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
         out = Value(self.val * other.val, (self, other), '*')
 
         def _backward():
@@ -27,7 +31,14 @@ class Value:
             other.grad += out.grad * self.val
         out._backward = _backward
         return out
+    
+    def __rmul__(self, other):
+        return self*other
+    def __sub__(self,other):
+        return self+(other*-1)
 
+    def __rsub__(self, other):
+        return self-other
     def relu(self):
         out = Value(max(0, self.val), (self,), 'relu')
         
@@ -35,6 +46,9 @@ class Value:
             self.grad += out.grad * (out.val > 0)
         out._backward = _backward
         return out
+    
+    def __neg__(self,x):
+        return self*-1
 
     def backward(self):
         ordered_computational_graph_nodes = []
@@ -43,7 +57,7 @@ class Value:
         def traverse_computational_graph(node):
             if node not in visited:
                 visited.add(node)
-                for child in node._prev:  # selects only op nodes
+                for child in node._prev:  # selects only 'op' nodes as only they have children
                     traverse_computational_graph(child)
                 
                 # recursively appends the elements in the computational graph into this array from left to right
@@ -59,12 +73,4 @@ class Value:
 
 if __name__ == '__main__':
     a = Value(2)
-    b = Value(3)
-    c = Value(4)
-    d = a + b * c
-    e = Value.relu(d)
-    e.backward()
-    print(a,b,d,c,d,e)
-
-
-
+    print(a)
